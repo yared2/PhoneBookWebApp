2168597ed728563bf8972c65cae1e48e8d7f909d
yared2 <yared555@gmail.com>
2021-02-16 22:34:00 -0600

    initial commit

---
+++ src/main/java/com/AshokIt/PhoneBookWebApp/PhoneBookWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.AshokIt.PhoneBookWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneBookWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoneBookWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AshokIt/PhoneBookWebApp/PhoneBookWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AshokIt.PhoneBookWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneBookWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
